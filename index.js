8d4267c88848e2a7e15234faaab356c5767a5557
Egar19 <madeeegar@gmail.com>
2025-04-12 12:25:39 +0800

    merapihkan tampilan

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 const toggle = document.getElementById("darkModeToggle");
+const icon = document.getElementById("icon")
+const copyright = document.getElementById("copyright")
+
 toggle.addEventListener("click", () => {
     document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains('dark-mode')) {
+        icon.src = 'assets/sun.svg';  // Tampilkan ikon matahari
+        icon.alt = 'Ganti ke Light Mode';
+    } else {
+        icon.src = 'assets/moon.svg'; // Tampilkan ikon bulan
+        icon.alt = 'Ganti ke Dark Mode';
+    }
+
 });
 
+const d = new Date();
+let year = d.getFullYear();
+
+copyright.innerHTML = `Made Egar @${year}`
